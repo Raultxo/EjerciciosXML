0d528c7c0afef22c10ed6d47accaac3b1aa3f4ac
Raultxo <raul6114@gmail.com>
2022-01-27 09:25:03 +0100

    Ejercicio 1 terminado

---
+++ app/src/main/java/com/example/ejerciciosxml/RssParserSAXSimplificado.java
@@ -0,0 +1,60 @@
+package com.example.ejerciciosxml;
+
+import android.sax.Element;
+import android.sax.RootElement;
+import android.util.Xml;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RssParserSAXSimplificado {
+
+    private final URL rssUrl;
+    private Noticia noticiaActual;
+    public RssParserSAXSimplificado(String url) {
+        try {
+            this.rssUrl = new URL (url);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Noticia> parse(){
+        final List<Noticia> noticias = new ArrayList<>();
+        RootElement root = new RootElement("rss");
+        Element channel = root.getChild("channel");
+        Element item = channel.getChild("item");
+        item.setStartElementListener(attributes -> noticiaActual = new Noticia());
+        item.setEndElementListener(() -> noticias.add(noticiaActual));
+        item.getChild("title").setEndTextElementListener(
+                body -> noticiaActual.setTitulo(body));
+        item.getChild("link").setEndTextElementListener(
+                body -> noticiaActual.setLink(body));
+        item.getChild("description").setEndTextElementListener(
+                body -> noticiaActual.setDescripcion(body));
+        item.getChild("guid").setEndTextElementListener(
+                body -> noticiaActual.setGuid(body));
+        item.getChild("pubDate").setEndTextElementListener(
+                body -> noticiaActual.setFecha(body));
+        try {
+            Xml.parse(this.getInputStream(),
+                    Xml.Encoding.UTF_8,
+                    root.getContentHandler());
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        return noticias;
+    }
+
+    private InputStream getInputStream() {
+        try {
+            return rssUrl.openConnection().getInputStream();
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
